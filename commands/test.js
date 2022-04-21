15a32b8af1bc5f936f919345227521774a70b0a3
yoy0lol <94529498+yoy0lol@users.noreply.github.com>
2022-04-21 23:51:00 +0300

    Updated test.js

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,23 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const data = new SlashCommandBuilder()
-	.setName('echo')
-	.setDescription('Replies with your input!')
-	.addStringOption(option =>
-		option.setName('input')
-			.setDescription('The input to echo back')
-			.setRequired(true));
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('Replies with your input!')
+		.addStringOption(option =>
+			option.setName('text')
+			.setDescription('The input to echo back!')
+			.setRequired(true)
+			)
+		.addStringOption(option =>
+			option.setName('text2')
+			.setDescription('another thing')
+			.setRequired(true)
+			)
+		,
+	async execute(interaction) {
+		const input1 = interaction.options.getString('text')
+		const input2 = interaction.options.getString('text2')
+		await interaction.reply(`${input1} and ${input2}`);
+	},
+};
