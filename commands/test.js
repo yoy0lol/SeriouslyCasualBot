8ab0e8b9ad66ac6dcfb09372e2b4eafa72ea5d38
yoy0lol <94529498+yoy0lol@users.noreply.github.com>
2022-04-24 01:25:45 +0300

    Added a new sub-command: view
    - Added the view sub-command, includes being able to filter out content to get the video based on user request.

--- commands/test.js
+++ commands/test.js
@@ -1,15 +1,24 @@
 const { databaseString } = require('../config.json');
 const Keyv = require('keyv');
-
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { DiscordAPIError } = require('discord.js');
 const failVideos = new Keyv(databaseString, { namespace:`failvideos` });
 failVideos.on('error', err => console.error('Keyv connection error:', err));
 
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { DiscordAPIError } = require('discord.js');
+// FUNCTIONS
 
 function pushToList(list, ob){
 	return list.push(ob)
 }
+
+function urlChecker(link){
+	const urlSourceRegexTest = /twitch|youtube|squad/
+	const urlCheckTest = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+	return urlSourceRegexTest.test(link) && urlCheckTest.test(link)	
+}
+
+// CODE
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -36,77 +45,119 @@ module.exports = {
 						))
 		.addSubcommand(subcommand =>
 				subcommand
-					.setName('view')
-					.setDescription("View a player's fails")
+					.setName('list')
+					.setDescription("List a player's fails")
 					.addUserOption(option =>
 						option.setName('name')
 						.setDescription('The discord user tag of the player')
 						.setRequired(true))
 					)
+		.addSubcommand(subcommand =>
+				subcommand
+					.setName('view')
+					.setDescription("View a player's fail video")
+					.addUserOption(option =>
+						option.setName('name')
+						.setDescription('The discord user tag of the player')
+						.setRequired(true))
+					.addNumberOption(option =>
+						option.setName('id')
+						.setDescription("The ID of the player's fail video")
+						.setRequired(true))
+					)
 		,
 	
-
-	
-
-
 	async execute(interaction) {
+		const name = interaction.options.getUser('name')
+		const failUrl = interaction.options.getString('url')
+		const desc = interaction.options.getString('description')
+		let vidList = await failVideos.get(name.id)
+
+		// CODE FOR SUBCOMMAND: ADD
+
 		if (interaction.options.getSubcommand() === 'add') {
-			console.log (`this is the ADD subcommand`)
+			// regex check for link
+			const linkValid = urlChecker(failUrl)
+			// TODO: desc character limit?
+			// TODO: if the same URL is there, decline?
+			
+			// if the regex validation passes, continue with the code, else alert the user that this is invalid:
+			if (linkValid) {
+				// set a variable that can change
+				let id = 1
+				
+				// if it doesnt exist, create an empty list
+				if (!vidList) {
+					vidList = []
+					pushToList(vidList, {id, failUrl, desc})
+				} else {
+					id = vidList[vidList.length - 1].id + 1
+					pushToList(vidList, {id, failUrl, desc})
+				}
+				
+				await failVideos.set(name.id, vidList)
+				
+				const addedVideoReply = `
+				Your video has been added!\n\nHere's a fail video of ${name} with this description:\n${desc}\n\n${failUrl}`
+				
+				await interaction.reply({
+					content: addedVideoReply,
+					ephemeral: false,
+				})
+			} else {
+				await interaction.reply({
+					content: '⚠️ERROR⚠️\nThis is not a valid link!\n\nMake sure the following conditions are met:\n1. The link is from YouTube, Twitch or Squad.OV\n2. The link is a valid URL.',
+					ephemeral: true
+				})
+			}
 		}
+		
+		// CODE FOR SUBCOMMAND: LIST
+		
+		if (interaction.options.getSubcommand() === 'list') {
+			if (vidList) {
+				let idFieldValues = ''
+				let vidFieldValues = ''
+				vidList
+				.reverse()
+				.forEach(vidEntry => {
+					console.log(vidEntry.id, vidEntry.failUrl, vidEntry.desc)
+					idFieldValues += `${vidEntry.id}\n\n`
+					vidFieldValues += `${vidEntry.desc}\n\n`
+				});
+				console.log(name)
+
+				const failVideoListEmbed = new MessageEmbed()
+				.setTitle(`${name.username}'s Fail Video List`)
+				.addFields(
+					{name: 'ID', value: idFieldValues, inline: true},
+					{name: 'Description', value: vidFieldValues, inline: true}
+				)
+
+				await interaction.reply({
+					content: `Here are ${name}'s fail videos. To get the link for them, use the following command: /fail view ${name} [ID number]`, // FIXME: make sure the command is the same
+					embeds: [failVideoListEmbed]
+				})
+
+			} else {
+				await interaction.reply({
+					content: `${name} doesn't have any fail videos. :(`,
+					ephemeral: true
+				})
+			}
+		}
+
+		// CODE FOR SUBCOMMAND: VIEW
 		if (interaction.options.getSubcommand() === 'view') {
-			console.log (`this is the VIEW subcommand`)
+			const filterId = interaction.options.getNumber('id')
+			if (vidList) {
+				const result = vidList.filter((e) => e.id === filterId)[0]
+				console.log(result)
+				await interaction.reply({
+					content: `Here is ${name}'s fail video: ${result.failUrl}` //FIXME: Put a check in place so that when a user's input is invalid (i.e. when they put an ID that's not there, return an error message)
+				})
+			}
 		}
 
-		// const name = interaction.options.getUser('name')
-		// const failUrl = interaction.options.getString('url')
-		// const desc = interaction.options.getString('description')
-
-		// // regex check for link
-		// const urlSourceRegexTest = /twitch|youtube|squad/
-		// const urlCheckTest = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-		// const linkValid = urlSourceRegexTest.test(failUrl) && urlCheckTest.test(failUrl)
-		// console.log(linkValid)
-		
-		// // if the regex validation passes, continue with the code, else alert the user that this is invalid:
-		// if (linkValid) {
-		// 	// set a variable that can change
-		// 	let id = 1
-		// 	let vidList = await failVideos.get(name.id)
-			
-		// 	// if it doesnt exist, create an empty list
-		// 	if (!vidList) {
-		// 		vidList = []
-		// 		pushToList(vidList, {id, failUrl, desc})
-		// 	} else {
-		// 		id = vidList[vidList.length - 1].id + 1
-		// 		pushToList(vidList, {id, failUrl, desc})
-		// 	}
-			
-		// 	await failVideos.set(name.id, vidList)
-			
-		// 	const addedVideoReply = `
-		// 	Your video has been added!\n\nHere's a fail video of ${name} with this description:\n${desc}\n\n${failUrl}`
-			
-		// 	await interaction.reply({
-		// 		content: addedVideoReply,
-		// 		ephemeral: false,
-		// 	})
-		// } else {
-		// 	await interaction.reply({
-		// 		content: '⚠️ERROR⚠️\nThis is not a valid link!\n\nMake sure the following conditions are met:\n1. The link is from YouTube, Twitch or Squad.OV\n2. The link is a valid URL.',
-		// 		ephemeral: true
-		// 	})
-		// }
-
-
-
-		
-
-
-
-		// TODO: find a way to assign an id to each entry (1, 2, 3...) ✅
-		// TODO: Add a validation check to the URL ✅
-		// TODO: Find a way to react to a specific subcommand
-		// TODO: add a sub option (to list the videos 'Yoyo's fail videos are..)
 	},
 };
